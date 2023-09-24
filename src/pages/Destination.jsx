5d08c0ccf549592351b596b90824d77d6fb10e77
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-09-24 23:32:05 +0100

    feat: destination desktop page setup complete

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -19,8 +19,8 @@ const Destination = () => {
   return (
     <div className='min-h-[100vh] p-4 bg-destination-mobile bg-cover text-white md:bg-destination-tablet md:pt-[0] md:pr-[0] xl:bg-destination-desktop'>
       <Header />
-      <main className='py-4 grid gap-5 md:mt-10 md:px-6'>
-        <h1 className='font-tagline text-lg text-center text-white uppercase tracking-widest md:text-[2rem] md:text-left'>
+      <main className='py-4 grid gap-5 md:mt-10 md:px-6 xl:grid-cols-2 xl:w-[120rem] xl:mx-auto'>
+        <h1 className='font-tagline text-lg text-center text-white uppercase tracking-widest md:text-[2rem] md:text-left xl:text-[2.8rem]'>
           <span className='mr-2 font-bold text-gray'>01</span>
           Pick your destination
         </h1>
@@ -28,11 +28,11 @@ const Destination = () => {
         <img
           src={currDestination.images.webp}
           alt={currDestination.name}
-          className='w-[17rem] h-[17rem] mx-auto md:w-[30rem] md:h-[30rem]'
+          className='w-[17rem] h-[17rem] mx-auto md:w-[30rem] md:h-[30rem] xl:col-start-1 xl:w-[45rem] xl:h-[45rem] xl:self-center'
         />
 
-        <section className='w-[30rem] mx-auto grid gap-4 md:w-[57.5rem] md:grid-cols-2'>
-          <ul className='flex justify-center gap-4 md:col-span-full'>
+        <section className='w-[30rem] mx-auto grid gap-4 md:w-[57.5rem] md:grid-cols-2 xl:col-start-2 xl:w-[45rem]'>
+          <ul className='flex justify-center gap-4 md:col-span-full xl:justify-start'>
             <li
               className={`py-2 font-tagline ${
                 index == 0
@@ -87,29 +87,25 @@ const Destination = () => {
             </li>
           </ul>
 
-          <div className='pb-6 border-b border-b-gray md:pb-10 md:col-span-full'>
-            <h2 className='font-display uppercase text-center text-clamp'>
+          <div className='pb-6 border-b border-b-gray text-center md:pb-10 md:col-span-full xl:text-start'>
+            <h2 className='font-display uppercase text-clamp xl:text-[10rem]'>
               {currDestination.name}
             </h2>
 
-            <p className='text-lg text-blue text-center'>
+            <p className='text-lg text-blue text-[1.8rem]'>
               {currDestination.description}
             </p>
           </div>
 
-          <div className='grid justify-center font-display md:mt-4'>
-            <h3 className='font-tagline text-blue text-sm uppercase text-center tracking-widest'>
-              Avg. Distance
-            </h3>
+          <div className='grid justify-center font-display uppercase text-center tracking-widest md:mt-4 xl:justify-start xl:text-start'>
+            <h3 className='font-tagline text-blue text-sm '>Avg. Distance</h3>
             <span className='text-[2.8rem] uppercase'>
               {currDestination.distance}
             </span>
           </div>
 
-          <div className='grid justify-center font-display  md:mt-4'>
-            <h3 className='font-tagline text-blue text-sm uppercase text-center tracking-widest'>
-              Est. Travel Time
-            </h3>
+          <div className='grid justify-center font-display uppercase text-center tracking-widest md:mt-4 xl:justify-start xl:text-start'>
+            <h3 className='font-tagline text-blue text-sm'>Est. Travel Time</h3>
             <span className='text-[2.8rem] uppercase'>
               {currDestination.travel}
             </span>
